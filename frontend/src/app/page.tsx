b05332d79c4cfc7345003dae00a6b02b3d3278bf
George Kephart <gkephart@cnm.edu>
2024-02-08 08:16:25 -0700

    added a new header to landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function() {
         <>
 
             <main className="hero min-h-screen py border-rounded" >
+                <h1>If i go to x.com and I get redirected to twitter.com its still twitter.</h1>
                 <div className="hero-content border-2 rounded-2xl border-neutral-content flex bg-neutral gap-10  border-roun flex-col md:flex-row">
                     <Image className="" src={"/login-hero.jpg"} alt="person holding phone staring at twitter login page Photo by Akshar Dave🌻 on Unsplash" priority={true} width={300} height={450}></Image>
                     <div className={""}>
